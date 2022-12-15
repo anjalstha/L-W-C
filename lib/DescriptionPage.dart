cf5a50d1a253695b9831d5402a6ac4e9eba27579
anjalstha <lostnjl@gmail.com>
2022-12-15 13:39:17 +0545

    2

---
+++ lib/DescriptionPage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DescriptionPage extends StatelessWidget {
+  const DescriptionPage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('hey, hello'),
+            Image.asset(
+              'images/najeek.png',
+              width: 400,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lwc/DescriptionPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,113 +13,126 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: const MyHomePage(title: 'List'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController serachController = TextEditingController();
-  String search = '';
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(0),
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://images.unsplash.com/photo-1484876065684-b683cf17d276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
-              fit: BoxFit.cover,
-            ),
-          ),
+        backgroundColor: Colors.grey.shade800,
+        appBar: AppBar(
+          title: const Text('Lyrics With Chords'),
+        ),
+        body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 0,
+              vertical: 10.0,
+            ),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  controller: serachController,
-                  decoration: const InputDecoration(
-                    hintText: 'Search',
-                    border: const OutlineInputBorder(),
-                  ),
-                  onChanged: (String? value) {
-                    print(value);
-                    setState(() {
-                      search = value.toString();
-                    });
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return const DescriptionPage(
+                            title: 'Najeek',
+                          );
+                        },
+                      ),
+                    );
                   },
+                  child: Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 5.0,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade400,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                            offset: Offset(
+                              2,
+                              2,
+                            ),
+                          )
+                        ]),
+                    child: Column(
+                      children: const [
+                        ListTile(
+                          title: Text('Najeek'),
+                          trailing: Icon(Icons.arrow_forward_ios),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: 500,
-                      itemBuilder: (context, index) {
-                        late String position = index.toString();
-                        if (serachController.text.isEmpty) {
-                          return ListTile(
-                            leading: const CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  'https://images.unsplash.com/photo-1619983081563-430f63602796?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
-                            ),
-                            title: Text('A S' + index.toString(),
-                                style: TextStyle(
-                                  color: Colors.white,
-                                )),
-                            subtitle: const Text('Anjal Shrestha',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                )),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return const DescriptionPage(
+                            title: 'Muskurayera',
                           );
-                        } else if (position
-                            .toLowerCase()
-                            .contains(serachController.text.toLowerCase())) {
-                          return ListTile(
-                            leading: const CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  'https://images.unsplash.com/photo-1619983081563-430f63602796?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                        },
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 5.0,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade400,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                            offset: Offset(
+                              2,
+                              2,
                             ),
-                            title: RichText(
-                                text: TextSpan(
-                                    text: 'A S',
-                                    style: DefaultTextStyle.of(context).style,
-                                    children: <TextSpan>[
-                                  TextSpan(
-                                      text: index.toString(),
-                                      style: TextStyle(
-                                        color: Colors.red,
-                                      )),
-                                ])),
-                            subtitle: const Text('Anjal Shrestha'),
-                          );
-                        } else {
-                          return Container();
-                        }
-                      }),
-                )
+                          )
+                        ]),
+                    child: Column(
+                      children: const [
+                        ListTile(
+                          title: Text('Muskurayera'),
+                          trailing: Icon(Icons.arrow_forward_ios),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
